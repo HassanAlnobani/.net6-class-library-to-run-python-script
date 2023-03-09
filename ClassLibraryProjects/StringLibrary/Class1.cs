1a626f77952c6620ff0dd401130d4aa6a7d7b0f6
Hassan Alnobani <116947469+HassanAlnobani@users.noreply.github.com>
2023-03-09 12:47:21 +0300

    Add files via upload

---
+++ ClassLibraryProjects/StringLibrary/Class1.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace StringLibrary;
+public class Class1
+{
+    public string Run(string cmd, string args)
+    {
+        System.Diagnostics.ProcessStartInfo start = new System.Diagnostics.ProcessStartInfo();
+        start.FileName = "python";
+        start.Arguments = string.Format("\"{0}\" \"{1}\"", cmd, args);
+        start.UseShellExecute = false;
+        start.CreateNoWindow = true; 
+        start.RedirectStandardOutput = true;
+        start.RedirectStandardError = true; 
+        using (System.Diagnostics.Process process = System.Diagnostics.Process.Start(start))
+        {
+            using (StreamReader reader = process.StandardOutput)
+            {
+                string stderr = process.StandardError.ReadToEnd(); 
+                string result = reader.ReadToEnd(); 
+                return result;
+            }
+        }
+    }
+
+          public static void Main(string[] args)
+            {
+                var res = new Class1().Run("Face Mask Detection.ipynb","params");
+                    Console.WriteLine(res);
+            }
+
+}
+
